f7f057664737202595a3d75868a0b07d817067bc
salt <arcaorm00@gmail.com>
2020-10-13 14:22:34 +0900

    ui

--- src/app/navigations.js
+++ src/app/navigations.js
@@ -1,90 +1,95 @@
 export const navigations = [
   {
-    name: "Dashboard",
+    name: "KOSPI",
     path: "/dashboard/analytics",
     icon: "dashboard"
   },
   {
-    name: "Forms",
+    name: "NASDOQ",
+    path: "/dashboard/analytics",
+    icon: "dashboard"
+  },
+  {
+    name: "게시판",
     icon: "description",
     children: [
       {
-        name: "Basic",
+        name: "글 목록",
         path: "/forms/basic",
         iconText: "B"
       },
       {
-        name: "Editor",
+        name: "글쓰기",
         path: "/forms/editor",
         iconText: "E"
       }
     ]
   },
   {
-    name: "Drag and Drop",
+    name: "관심 종목",
     icon: "control_camera",
     path: "/others/drag-and-drop"
   },
+  // {
+  //   name: "Multilevel",
+  //   icon: "trending_up",
+  //   children: [
+  //     {
+  //       name: "Level 1",
+  //       icon: "list",
+  //       children: [
+  //         {
+  //           name: "Item 1",
+  //           path: "/charts/victory-charts",
+  //           iconText: "1"
+  //         },
+  //         {
+  //           name: "Item 2",
+  //           path: "/charts/react-vis",
+  //           iconText: "2"
+  //         },
+  //         {
+  //           name: "Item 3",
+  //           path: "/charts/recharts",
+  //           iconText: "3"
+  //         },
+  //         {
+  //           name: "Item 4",
+  //           path: "/charts/echarts",
+  //           iconText: "4"
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
   {
-    name: "Multilevel",
-    icon: "trending_up",
-    children: [
-      {
-        name: "Level 1",
-        icon: "list",
-        children: [
-          {
-            name: "Item 1",
-            path: "/charts/victory-charts",
-            iconText: "1"
-          },
-          {
-            name: "Item 2",
-            path: "/charts/react-vis",
-            iconText: "2"
-          },
-          {
-            name: "Item 3",
-            path: "/charts/recharts",
-            iconText: "3"
-          },
-          {
-            name: "Item 4",
-            path: "/charts/echarts",
-            iconText: "4"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: "Utilities",
+    name: "주가 예측",
     icon: "format_list_bulleted",
     children: [
       {
-        name: "Color",
+        name: "Samsung",
         path: "/utilities/color",
         iconText: "C"
       },
       {
-        name: "Spacing",
+        name: "Apple",
         path: "/utilities/spacing",
         iconText: "S"
       },
       {
-        name: "Typography",
+        name: "temp",
         path: "/utilities/typography",
         iconText: "T"
       },
       {
-        name: "Display",
+        name: "temp",
         path: "/utilities/display",
         iconText: "D"
       }
     ]
   },
   {
-    name: "Sessions",
+    name: "회원 관리",
     icon: "trending_up",
     children: [
       {
@@ -112,89 +117,89 @@ export const navigations = [
   
   
 
-  {
-    name: "UI Kits",
-    icon: "favorite",
-    badge: { value: "50+", color: "secondary" },
-    children: [
-      {
-        name: "Auto Complete",
-        path: "/material/autocomplete",
-        iconText: "A"
-      },
-      {
-        name: "Buttons",
-        path: "/material/buttons",
-        iconText: "B"
-      },
-      {
-        name: "Checkbox",
-        path: "/material/checkbox",
-        iconText: "C"
-      },
-      {
-        name: "Dialog",
-        path: "/material/dialog",
-        iconText: "D"
-      },
-      {
-        name: "Expansion Panel",
-        path: "/material/expansion-panel",
-        iconText: "E"
-      },
-      {
-        name: "Form",
-        path: "/material/form",
-        iconText: "F"
-      },
-      {
-        name: "Icons",
-        path: "/material/icons",
-        iconText: "I"
-      },
-      {
-        name: "Menu",
-        path: "/material/menu",
-        iconText: "M"
-      },
-      {
-        name: "Progress",
-        path: "/material/progress",
-        iconText: "P"
-      },
-      {
-        name: "Radio",
-        path: "/material/radio",
-        iconText: "R"
-      },
-      {
-        name: "Switch",
-        path: "/material/switch",
-        iconText: "S"
-      },
-      {
-        name: "Slider",
-        path: "/material/slider",
-        iconText: "S"
-      },
-      {
-        name: "Snackbar",
-        path: "/material/snackbar",
-        iconText: "S"
-      },
-      {
-        name: "Table",
-        path: "/material/table",
-        iconText: "T"
-      }
-    ]
-  },
+  // {
+  //   name: "UI Kits",
+  //   icon: "favorite",
+  //   badge: { value: "50+", color: "secondary" },
+  //   children: [
+  //     {
+  //       name: "Auto Complete",
+  //       path: "/material/autocomplete",
+  //       iconText: "A"
+  //     },
+  //     {
+  //       name: "Buttons",
+  //       path: "/material/buttons",
+  //       iconText: "B"
+  //     },
+  //     {
+  //       name: "Checkbox",
+  //       path: "/material/checkbox",
+  //       iconText: "C"
+  //     },
+  //     {
+  //       name: "Dialog",
+  //       path: "/material/dialog",
+  //       iconText: "D"
+  //     },
+  //     {
+  //       name: "Expansion Panel",
+  //       path: "/material/expansion-panel",
+  //       iconText: "E"
+  //     },
+  //     {
+  //       name: "Form",
+  //       path: "/material/form",
+  //       iconText: "F"
+  //     },
+  //     {
+  //       name: "Icons",
+  //       path: "/material/icons",
+  //       iconText: "I"
+  //     },
+  //     {
+  //       name: "Menu",
+  //       path: "/material/menu",
+  //       iconText: "M"
+  //     },
+  //     {
+  //       name: "Progress",
+  //       path: "/material/progress",
+  //       iconText: "P"
+  //     },
+  //     {
+  //       name: "Radio",
+  //       path: "/material/radio",
+  //       iconText: "R"
+  //     },
+  //     {
+  //       name: "Switch",
+  //       path: "/material/switch",
+  //       iconText: "S"
+  //     },
+  //     {
+  //       name: "Slider",
+  //       path: "/material/slider",
+  //       iconText: "S"
+  //     },
+  //     {
+  //       name: "Snackbar",
+  //       path: "/material/snackbar",
+  //       iconText: "S"
+  //     },
+  //     {
+  //       name: "Table",
+  //       path: "/material/table",
+  //       iconText: "T"
+  //     }
+  //   ]
+  // },
   
-  {
-    name: "Map",
-    icon: "add_location",
-    path: "/map"
-  },
+  // {
+  //   name: "Map",
+  //   icon: "add_location",
+  //   path: "/map"
+  // },
   
   
 ];
